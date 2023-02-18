985b9b755a64c57995f9ca99f5aa1dd9d65e506e
ashimMarch <104412075+ashimMarch@users.noreply.github.com>
2023-02-18 13:09:33 +0530

    LayoutBuilder used as top widget
    set responsive width and height

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,12 +1,35 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: LayoutBuilder(// we need to know how much width and height available for this car after taken AppBar and NavBar space
+          builder: (context, constrains) {
+            print('constrains.maxHeight : ${constrains.maxHeight} ');
+            print('constrains.minHeight : ${constrains.minHeight}');
+            print('constrains.maxWidth : ${constrains.maxWidth}');
+            print('constrains.minWidth : ${constrains.minWidth}');
+            print('\n ${constrains.maxHeight*0.1}\n');
+            return Stack(
+              children: [
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: constrains.maxHeight*0.1),
+                  child: SvgPicture.asset(
+                    'assets/icons/Car.svg',
+                    width: double.infinity,
+                    height: double.infinity,
+                  ),
+                ),
+              ],
+            );
+          }
+        ),
+      ),
+    );
   }
 }
