f6cc7e21ecb8156dfdab206be0b1ce33f0975463
ashimMarch <104412075+ashimMarch@users.noreply.github.com>
2023-02-19 11:29:38 +0530

    converted to StatefulWidget, battery fade in fade out completed
    mixin used, animation and animation controller created

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,19 +5,58 @@ import 'package:animated_car/home_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   HomeScreen({super.key});
 
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateMixin {
   final HomeController _controller = HomeController();
+
+  late AnimationController _batteryAnimationController;
+  late Animation<double> _animationBattery;
+
+  void setupBatteryAnimation(){
+    _batteryAnimationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 600)
+    );
+    _animationBattery = CurvedAnimation(
+      parent: _batteryAnimationController, 
+      // so this animation start at 0 and end on half
+      // means after 300 milliseconds [total duration is 600]
+      curve: const Interval(0.0, 0.5),
+    );
+  }
+  @override
+  void initState() {
+    super.initState();
+    setupBatteryAnimation();
+  }
+  @override
+  void dispose() {
+    _batteryAnimationController.dispose();
+    super.dispose();
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return AnimatedBuilder(
-      animation: _controller,
+      animation: Listenable.merge([_controller,_batteryAnimationController]),
       builder: (context, _) {
+        print('${_animationBattery.value} \n');
         return Scaffold(
           bottomNavigationBar: TeslaBottomNavigationBar(
             selectedTab: _controller.selectedBottomTab,
             onTap: (index) {
+              if(index == 1) {
+                _batteryAnimationController.forward();
+              } else if(_controller.selectedBottomTab == 1 && index != 1){
+                _batteryAnimationController.reverse(from: 0.7);
+              }
               _controller.onBottomNavigationTabChange(index);
             },
           ),
@@ -47,25 +86,47 @@ class HomeScreen extends StatelessWidget {
                         ),
                       ),
                     ),
-                    Positioned(
-                      left: constrains.maxWidth*0.05,
-                      child: DoorLock(
-                        isLock: _controller.isLeftDoorLocked,
-                        press: _controller.updateLeftDoorLock, 
+                    AnimatedPositioned(
+                      duration: defaultDuration,
+                      left: _controller.selectedBottomTab == 0 ? constrains.maxWidth*0.05 : constrains.maxWidth*0.5,
+                      child: AnimatedOpacity(
+                        duration: defaultDuration,
+                        opacity: _controller.selectedBottomTab == 0 ? 1 : 0,
+                        child: DoorLock(
+                          isLock: _controller.isLeftDoorLocked,
+                          press: _controller.updateLeftDoorLock, 
+                        ),
                       ),
                     ),
-                    Positioned(
-                      top: constrains.maxHeight*0.13,
-                      child: DoorLock(
-                        isLock: _controller.isBonnetLock,
-                        press: _controller.updateBonnetDoorLock, 
+                    AnimatedPositioned(
+                      duration: defaultDuration,
+                      top: _controller.selectedBottomTab == 0 ? constrains.maxHeight*0.13 : constrains.maxHeight*0.5,
+                      child: AnimatedOpacity(
+                        duration: defaultDuration,
+                        opacity: _controller.selectedBottomTab == 0 ? 1 : 0,
+                        child: DoorLock(
+                          isLock: _controller.isBonnetLock,
+                          press: _controller.updateBonnetDoorLock, 
+                        ),
                       ),
                     ),
-                    Positioned(
-                      bottom: constrains.maxHeight*0.17,
-                      child: DoorLock(
-                        isLock: _controller.isTrunkLock,
-                        press: _controller.updateTrunkDoorLock, 
+                    AnimatedPositioned(
+                      duration: defaultDuration,
+                      bottom: _controller.selectedBottomTab == 0 ? constrains.maxHeight*0.17 : constrains.maxHeight*0.5,
+                      child: AnimatedOpacity(
+                        duration: defaultDuration,
+                        opacity: _controller.selectedBottomTab == 0 ? 1 : 0,
+                        child: DoorLock(
+                          isLock: _controller.isTrunkLock,
+                          press: _controller.updateTrunkDoorLock, 
+                        ),
+                      ),
+                    ),
+                    Opacity(
+                      opacity: _animationBattery.value,
+                      child: SvgPicture.asset(
+                        'assets/icons/Battery.svg',
+                        width: constrains.maxWidth*0.45,
                       ),
                     ),
                   ],
